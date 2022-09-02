700894f759efe54840cb7e047bedbc654319892e
Dmitry <Logan6338@mail.ru>
2022-09-02 20:28:12 +0300

    Задача 63

---
+++ Task01/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите число N");
+int N = Convert.ToInt32(Console.ReadLine());
+
+string s = string.Empty;
+
+string FillStringInNumDiapazone (string c, int Num)
+{
+    c = "1 ";
+    if (Num == 1)
+    {
+        return c;
+    }
+
+    else
+    {
+       c = FillStringInNumDiapazone(c, Num - 1) + $"{Convert.ToString(Num)} ";
+    }
+    return c;
+}
+
+string result = FillStringInNumDiapazone(s, N);
+
+Console.WriteLine(result);
