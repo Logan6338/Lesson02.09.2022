b56715ac8292197c0e43e363302e6aca884fcb33
Dmitry <Logan6338@mail.ru>
2022-09-02 21:52:20 +0300

    Задача 69 Двумя методами

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -19,3 +19,31 @@ int MathPow(int a, int Pow, int index = 1)
 }
 
 Console.WriteLine(MathPow(inputNum, inputPow));
+
+/*
+
+Второй вариант решения задачи
+
+Console.Clear();
+Console.WriteLine("Введите число А");
+int inputNum = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите степень В");
+int inputPow = Convert.ToInt32(Console.ReadLine());
+
+int MathPow(int a, int Pow)
+{
+    int result = a;
+    if (Pow > 0)
+    {
+        result = result * MathPow(a, Pow -1);
+    }
+    
+    else
+    {
+        return 1;
+    }
+    return result;
+}
+
+Console.WriteLine(MathPow(inputNum, inputPow));
+*/
