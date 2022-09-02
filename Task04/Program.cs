c618a23f526883a05df9f982b40a4239b3f99301
Dmitry <Logan6338@mail.ru>
2022-09-02 21:40:10 +0300

    Задача 69

---
+++ Task04/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите число А");
+int inputNum = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите степень В");
+int inputPow = Convert.ToInt32(Console.ReadLine());
+
+int MathPow(int a, int Pow, int index = 1)
+{
+    int result = a;
+    if (Pow == 0)
+    {
+        return 1;
+    }
+
+    else if (index < Pow)
+    {
+        result = result * MathPow(a, Pow, index + 1);
+    }
+    return result;
+}
+
+Console.WriteLine(MathPow(inputNum, inputPow));
