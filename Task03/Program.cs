326e6cf1bc04df7f0eb2387fdf1d8b54aad15701
Dmitry <Logan6338@mail.ru>
2022-09-02 21:25:39 +0300

    Задача 67

---
+++ Task03/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Введите число N");
+string inputString = Console.ReadLine();
+char[] arr = inputString.ToCharArray();
+
+int NumLenght = arr.Length;
+
+int FillSumNum(char[] array, int IntIndex)
+{
+    int Sum = 0;
+
+    if (IntIndex == 0)
+    {
+        Sum = (array[IntIndex] - '0');
+        
+        return Sum;
+    }
+    else
+    {
+
+        Sum = (arr[IntIndex]-'0') + FillSumNum(arr, IntIndex - 1);
+        
+    }
+    return Sum;
+}
+
+int result = FillSumNum(arr, NumLenght - 1);
+Console.WriteLine(result);
